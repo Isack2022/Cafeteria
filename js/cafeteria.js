e31a8efd7377cfb8488f42a17509db0133641307
Isack20222 <isackcsantos.dev@gmail.com>
2024-08-27 19:10:34 -0300

    Feature: Arrumando o cardápio

--- js/cafeteria.js
+++ js/cafeteria.js
@@ -1,3 +1,7 @@
+// Variavel com o conteudo inicial cafés para a rolagem dos
+// itens do cardápio
+let tp = "#Cafes";
+
 document.getElementsByClassName("btn-mobile")[0].onclick =() =>{
     document.getElementsByTagName("nav")[0].style.marginLeft = "0px";
 }; 
@@ -8,52 +12,62 @@ function closeMenu(){
     document.querySelector(".cat-cardapio a span").style.color = "white";
     document.getElementById("Bolos").style.display= "none";
     document.getElementById("Sobremesa").style.display= "none";
-    document.getElementById("Salgados").style.display= "none";
     document.getElementById("Bebidas").style.display="none";
+    document.getElementById('Salgados').style.display="none";
 }
 
-function mudaItem(cat){
+function mudarItem(cat){
     switch(cat){
         case "Cafes":
-            document.getElementById(cat).style.display = "Inline-flex"
-            document.getElementById("Bolos").style.display= "none";
-            document.getElementById("Sobremesa").style.display= "none";
-            document.getElementById("Salgados").style.display= "none";
-            document.getElementById("Bebidas").style.display="none";
+            document.getElementById(cat).style.display = "inline-flex";
+            document.getElementById("Bolos").style.display = "none";
+            document.getElementById("Sobremesa").style.display = "none";
+            document.getElementById("Salgados").style.display = "none";
+            document.getElementById("B  ebidas").style.display = "none";
+            tp = "#Cafes"
             trocaSelecao(cat);
             break;
+
         case "Bolos":
             document.getElementById(cat).style.display = "Inline-flex"
             document.getElementById("Cafes").style.display= "none";
             document.getElementById("Sobremesa").style.display= "none";
             document.getElementById("Salgados").style.display= "none";
             document.getElementById("Bebidas").style.display="none";
+            tp = "#Bolos"
             trocaSelecao(cat);
             break;
+
         case "Sobremesa":
             document.getElementById(cat).style.display = "Inline-flex"
             document.getElementById("Cafes").style.display= "none";
             document.getElementById("Bolos").style.display= "none";
             document.getElementById("Salgados").style.display= "none";
             document.getElementById("Bebidas").style.display="none";
+            tp = "#Sobremesa"
             trocaSelecao(cat);
             break;
-        case "Salgados":
-                document.getElementById(cat).style.display = "Inline-flex"
-                document.getElementById("Cafes").style.display= "none";
-                document.getElementById("Bolos").style.display= "none";
-                document.getElementById("Sobremesa").style.display= "none";
-                document.getElementById("Bebidas").style.display="none";
-                trocaSelecao(cat);
-                break;
+
         case "Bebidas":
             document.getElementById(cat).style.display = "Inline-flex"
             document.getElementById("Cafes").style.display= "none";
             document.getElementById("Bolos").style.display= "none";
             document.getElementById("Salgados").style.display= "none";
             document.getElementById("Sobremesa").style.display="none";
+            tp = "#Bebidas"
             trocaSelecao(cat);
             break;
+
+        case "Salgados":
+            document.getElementById(cat).style.display = "Inline-flex"
+            document.getElementById("Cafes").style.display= "none";
+            document.getElementById("Bolos").style.display= "none";
+            document.getElementById("Sobremesa").style.display= "none";
+            document.getElementById("Bebidas").style.display="none";
+            tp = "#Salgados"
+            trocaSelecao(cat);
+            break;
+                
         }
     
     function trocaSelecao(cat){
@@ -77,9 +91,8 @@ function mudaItem(cat){
             document.getElementsByClassName("cat-cardapio")[4].style.backgroundColor = "#fff";
             document.querySelectorAll(".cat-cardapio a")[4].style.color = "black";
             document.querySelectorAll(".cat-cardapio a span")[4].style.color = "#38250A";
-
-            
         }
+
         else if(cat=="Bolos"){
             document.getElementsByClassName("cat-cardapio")[1].style.backgroundColor = "#532D10";
             document.querySelectorAll(".cat-cardapio a")[1].style.color = "white";
@@ -101,6 +114,7 @@ function mudaItem(cat){
             document.querySelectorAll(".cat-cardapio a")[4].style.color = "black";
             document.querySelectorAll(".cat-cardapio a span")[4].style.color = "#38250A";
         }
+
         else if(cat=="Sobremesa"){
             document.getElementsByClassName("cat-cardapio")[2].style.backgroundColor = "#532D10";
             document.querySelectorAll(".cat-cardapio a")[2].style.color = "white";
@@ -122,7 +136,8 @@ function mudaItem(cat){
             document.querySelectorAll(".cat-cardapio a")[4].style.color = "black";
             document.querySelectorAll(".cat-cardapio a span")[4].style.color = "#38250A";
         }
-        else if(cat=="Salgados"){
+
+        else if(cat=="Bebidas"){
             document.getElementsByClassName("cat-cardapio")[3].style.backgroundColor = "#532D10";
             document.querySelectorAll(".cat-cardapio a")[3].style.color = "white";
             document.querySelectorAll(".cat-cardapio a span")[3].style.color = "white";
@@ -135,15 +150,16 @@ function mudaItem(cat){
             document.querySelectorAll(".cat-cardapio a")[2].style.color = "black";
             document.querySelectorAll(".cat-cardapio a span")[2].style.color = "#38250A";
 
-            document.getElementsByClassName("cat-cardapio")[0].style.backgroundColor = "#fff";
-            document.querySelectorAll(".cat-cardapio a")[0].style.color = "black";
-            document.querySelectorAll(".cat-cardapio a span")[0].style.color = "#38250A";
-
             document.getElementsByClassName("cat-cardapio")[4].style.backgroundColor = "#fff";
             document.querySelectorAll(".cat-cardapio a")[4].style.color = "black";
             document.querySelectorAll(".cat-cardapio a span")[4].style.color = "#38250A";
+
+            document.getElementsByClassName("cat-cardapio")[0].style.backgroundColor = "#fff";
+            document.querySelectorAll(".cat-cardapio a")[0].style.color = "black";
+            document.querySelectorAll(".cat-cardapio a span")[0].style.color = "#38250A";
         }
-        else if(cat=="Bebidas"){
+
+        else if(cat=="Salgados"){
             document.getElementsByClassName("cat-cardapio")[4].style.backgroundColor = "#532D10";
             document.querySelectorAll(".cat-cardapio a")[4].style.color = "white";
             document.querySelectorAll(".cat-cardapio a span")[4].style.color = "white";
@@ -156,37 +172,47 @@ function mudaItem(cat){
             document.querySelectorAll(".cat-cardapio a")[2].style.color = "black";
             document.querySelectorAll(".cat-cardapio a span")[2].style.color = "#38250A";
 
-            document.getElementsByClassName("cat-cardapio")[3].style.backgroundColor = "#fff";
-            document.querySelectorAll(".cat-cardapio a")[3].style.color = "black";
-            document.querySelectorAll(".cat-cardapio a span")[3].style.color = "#38250A";
-
             document.getElementsByClassName("cat-cardapio")[0].style.backgroundColor = "#fff";
             document.querySelectorAll(".cat-cardapio a")[0].style.color = "black";
             document.querySelectorAll(".cat-cardapio a span")[0].style.color = "#38250A";
+
+            document.getElementsByClassName("cat-cardapio")[3].style.backgroundColor = "#fff";
+            document.querySelectorAll(".cat-cardapio a")[3].style.color = "black";
+            document.querySelectorAll(".cat-cardapio a span")[3 ].style.color = "#38250A";
         }
-        else{
-            document.getElementsByClassName("cat-cardapio")[5].style.backgroundColor = "#532D10";
-            document.querySelectorAll(".cat-cardapio a")[5].style.color = "white";
-            document.querySelectorAll(".cat-cardapio a span")[5].style.color = "white";
-        }
+        
+        
     }
 }
 
-//Obter o tamanho da tela e usar apenas a metade
+// //Obter o tamanho da tela e usar apenas a metade
 let metadeTela = window.screen.width / 2;
 
 let pos = 0;
 
 //Vamos obter a posicao do mouse
-document.body.onmousemove = () => {
+document.body.onmousemove = ()=>{
+
     let px = window.event.clientX; //Pega a posição horizontal do mouse
-    
+
     if(px > metadeTela){
-        document.querySelector("section").style.marginLeft = -pos + "px";
-        pos-=3;
+
+        if(document.querySelector(tp).style.marginLeft == "-335px"){
+            document.querySelector(tp).style.marginLeft = "-335px";
+        }
+        else{
+            document.querySelector(tp).style.marginLeft = pos + "px";
+            pos -=5;
+        }
     }
     else{
-        document.querySelector("section").style.marginLeft = pos + "px";
-        pos+=3;  
+        if(document.querySelector(tp).style.marginLeft=="10px"){
+            document.querySelector(tp).style.marginLeft ="10px";
+        }
+        else{
+            document.querySelector(tp).style.marginLeft = pos+"px";
+            pos+=5;
+        }  
     }
+    console.log(document.querySelector(tp).style.marginLeft);
 };
